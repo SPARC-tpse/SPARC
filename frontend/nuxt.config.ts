5b57019ffd98105fb8f07fc5a6f7f76cd0c965ff
Christian Schöps <christian.a.schoeps@gmx.de>
2025-11-27 15:25:32 +0100

    tailwindcss

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false
+  ssr: false,
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })

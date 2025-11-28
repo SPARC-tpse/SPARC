d6c045f8167f24a2fb11d9cbae1de729eb1eafa5
Jelly <67157454+jeder49@users.noreply.github.com>
2025-11-28 15:17:24 +0100

    update readme + change hello world example

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-const version = 2 + 1
+import { ref } from 'vue'
+
+const message = ref(`Hello Nuxt ${2 + 1}!!`)
+
+function changeMessage() {
+  message.value = "hello world"
+}
 </script>
 
 <template>
   <div class="hello">
-    Hello Nuxt {{ version }}!!
+    {{ message }}
   </div>
+
+  <button class="btn" @click="changeMessage">
+    Change Text
+  </button>
 </template>
 
 <style scoped>
@@ -13,5 +23,11 @@ const version = 2 + 1
   font-family: Arial, Helvetica, sans-serif;
   font-size: 3rem;
   padding: 2rem;
+}
+
+.btn {
+  margin-left: 2rem;
+  padding: 0.5rem 1rem;
+  font-size: 1.2rem;
 }
 </style>

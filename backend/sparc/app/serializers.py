11be13409d51f5ef4aa58138965dd9629999e430
Thilo195 <thilo.hagelstein@gmail.com>
2025-12-10 15:47:20 +0100

    added some getter functions into services.py

--- backend/sparc/app/serializers.py
+++ backend/sparc/app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from sparc.app.models import Order, Process, Resource, DisruptionType
+from sparc.app.models import Order, Process, Resource, DisruptionType, Disruption
+from .services import Database
 
 class ProcessSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,3 +23,18 @@ class DisruptionTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = DisruptionType
         fields = ["id", "name"]
+
+class DisruptionSerialzier(serializers.ModelSerializer):
+    resource_name = serializers.SerializerMethodField()
+    disruption_type_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Disruption
+        fields = ('id', 'name', 'start_date', 'end_date', 'resource', 'resource_name', 'type', 'disruption_type_name')
+        
+
+    def get_resource_name_by_id(self, obj: Disruption) -> str:
+        return Database.get_resource_name_by_id(obj.resource_id)
+    
+    def get_disruption_type_name(self, obj: Disruption) -> str:
+        return Database.get_disruption_type_name_by_id(obj.type_id)

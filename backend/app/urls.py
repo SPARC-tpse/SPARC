1d6be9addeb0d139d71739c3f9e970882303d86b
Thilo195 <thilo.hagelstein@gmail.com>
2026-01-30 16:08:39 +0100

    changed urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
-from .views import get_orders, create_order, update_order, delete_order
+from .views import get_orders, create_order, update_order, delete_order, get_disruptions, create_disruption, update_disruption, delete_disruption, get_disruptionTypes, get_resources
 
 urlpatterns = [
+
+    #Order
     path("orders/get_orders", get_orders, name="get_orders"),
     path("orders/create_order", create_order, name="create_order"),
-    path("orders/update_order", update_order, name="update_order"),
-    path("orders/delete_order", delete_order, name="delete_order"),
+    path("orders/update_order/<int:order_id>/", update_order, name="update_order"),
+    path("orders/delete_order/<int:order_id>/", delete_order, name="delete_order"),
+    
+    #Disruption
+    path("disruptions/get_disruptions", get_disruptions, name="get_disruptions"),
+    path("disruptions/create_disruption", create_disruption, name="create_disruption"),
+    path("disruptions/update_disruption/<int:disruption_id>/", update_disruption, name="update_disruption"),
+    path("disruptions/delete_disruption/<int:disruption_id>/", delete_disruption, name="delete_disruption"),
+    
+    #DisruptionTypes
+    path("disruptionTypes/get_disruptionTypes", get_disruptionTypes, name="get_disruptionTypes"),
+    
+    #Resources
+    path("resources/get_resources", get_resources, name="get_resources"),
 ]
